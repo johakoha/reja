73f353bc3972b50d6c1568c667e864d99cdf3c5d
johakoha <jakhongirturaev312@gmail.com>
2025-09-19 19:15:31 +0900

    feat: develop author router page

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express(); // Express ilovasini yaratish
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log("ERROR: ", err);
+    }else {
+        user = JSON.parse(data);
+    }
+});
 
 //1: Kirish code
-app.use(express.static("public")); // "public" folderi ochiq degani clientlar uchun
+app.use(express.static("public"));  // 'public' folderi ochiq degani clientlar uchun
 app.use(express.json()); // JSON formatdagi ma'lumotlarni object holatiga o'girib beradi
 app.use(express.urlencoded({ extended: true })); // form request yani traditional requestlarni qabul qilib oladi, express qabul qilishi uchun kerak
 
@@ -21,6 +31,11 @@ app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({ test: "success" });
 });
+
+app.get(`/author`,(req,res)=> {
+    res.render("author", {user: user});
+});
+
 app.get("/", function (req, res) {
     res.render("harid");
 });
