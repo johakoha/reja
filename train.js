78f23288968f1af7a963b09f6399aa64fe4ebb88
johakoha <jakhongirturaev312@gmail.com>
2025-09-23 19:01:14 +0900

    feat:MITASK-A completed

--- train.js
+++ train.js
@@ -1,38 +1,59 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yahshi talaba boling", // 0-20
-    "togri boshliq tanlang va koproq hato qiling", // 20-30
-    "uzingizga ishlashingizni boshlang", // 30-40
-    "siz kuchli bolgan narsalarni qiling", // 40-50
-    "yoshlarga investitsiya qiling", // 50-60
-    "endi dam oling, foydasi yoq endi", // 60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yahshi talaba boling", // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "uzingizga ishlashingizni boshlang", // 30-40
+//     "siz kuchli bolgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling, foydasi yoq endi", // 60
+// ];
 
-async function maslahatBering(a) {
-  if (typeof a !== "number") throw new Error("insert a number");
-  else if (a <= 20) return list[0];
-  else if (a > 20 && a <= 30) return list[1];
-  else if (a > 30 && a <= 40) return list[2];
-  else if (a > 40 && a <= 50) return list[3];
-  else if (a > 50 && a <= 60) return list[4];
-  else {
-    return new Promise((resolve, reject) => {
-      setInterval(() => {
-        resolve(list[5]);
-      }, 5000);
-    });
-  }
-}
+// // CALLBACK functions
+// function maslahatBering (a, callback) {
+//   if (typeof a !== "number") callback("Insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a < 20 && a <= 30) callback(null, list[1]);
+//   else if (a < 30 && a <= 40) callback(null, list[2]);
+//   else if (a < 40 && a <= 50) callback(null, list[3]);
+//   else if (a < 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 1000);
+//   }
+// }
 
-console.log("passed here 0");
-maslahatBering(70, (err, data) => {
-  if (err) console.log("ERROR:", err);
-  else {
-    console.log(data);
-  }
-});
-console.log("passed here 1");
+// console.log("passed here 0");
+// maslahatBering(70, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   else {
+//     console.log(data);
+//   }
+// });
+// console.log("passed here 1");
 
+
+// ASYNC functions
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       setInterval(() => {
+//         resolve(list[5]);
+//       }, 5000);
+//     });
+//   }
+// }
+
+
+
+
+//call via then/catch
 // then/catch
 // console.log("passed here 0");
 // maslahatBering(20)
@@ -44,10 +65,35 @@ console.log("passed here 1");
 // });
 // console.log("passed here 1");
 
+
+//call via async/await 
 // async/await
 // async function run() {
 //     let javob = await maslahatBering(25);
 //     console.log(javob);
 // }
 // run();
+
+
+
+
+// Masalani izohi
+// A-TASK
+
+//Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+// MASALAN countLetter("e", "engineer") 3ni return qiladi.
+
+// masalani yechimi:
+
+// The NOTORIOUS MMA - CONNOR MCGREGOR: shut your mouth and take my money!
+// "Precision beats power, timing beats speed."
+
+function countLetter(letter, word) {
+  return word.split('').filter(c => c === letter).length;
+}
+
+console.log("McGregor says:", countLetter("o", "notorious")); // 3 ta o harfi qatnashgan
+
+
+
 
