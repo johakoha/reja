3ead09a1ef6565c72bc6190195982b3d7973d966
johakoha <jakhongirturaev312@gmail.com>
2025-09-29 00:05:51 +0900

    feat:Frontend javascriptni qurish

--- app.js
+++ app.js
@@ -32,24 +32,17 @@ app.set("view engine", "ejs");
 
 
 //4: Routing code
-app.post("/create-item", (req, res) => {
-
-    //TODO: code with db here
+app.post("/create-item", (req, res) => { 
     console.log("user Entered/create-item");
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end("something went wrong");
-        } else {
-            res.end("successfully added");
-        }
-    })
+        res.json(data.ops[0]);
+    });
 });
 
 app.get("/author", (req, res) => {
     res.render("author", { user: user });
-})
+});
 
 
 app.get("/", function (req, res) {
