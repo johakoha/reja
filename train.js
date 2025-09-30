f4e1516f4bbe9d6483d55236911758e7d2b32604
johakoha <jakhongirturaev312@gmail.com>
2025-09-30 15:45:05 +0900

    feat:TASK-C completed

--- train.js
+++ train.js
@@ -132,16 +132,68 @@
 
 //Yechimi:
 
-function countIntegers(input) {
-  let count = 0;
+// function countIntegers(input) {
+//   let count = 0;
 
   
-    for (let index in input) {
-      if (input[index] >= 0 && input[index] <= 9) {
-        count++;
-      }
-    }
-   return count;
+//     for (let index in input) {
+//       if (input[index] >= 0 && input[index] <= 9) {
+//         count++;
+//       }
+//     }
+//    return count;
+// }
+
+// console.log(countIntegers("duhf3uhr38239d"));
+
+
+
+// TASK - C
+// Masalani izohi
+// Shop nomli class tuzing, va bu class 3 xill parametr qabul qilsin. Hamda classning quyidagdek 3'ta metodi bo'lsin: 1) qoldiq 2) sotish 3) qabul
+// Har bir metod ishga tushgan vaqtda log qilinsin
+//MASALAN:
+//const shop = new Shop(4, 5, 2)
+// shop.qoldiq();
+// natija qaytishi kerak: Hozir 20: 40'da 4'ta non, 5'ta lag'mon va 2'ta cola mavjud
+// shop.sotish("non", 3); & shop.qabul("cola", 4); & shop.qoldiq();
+// Natija qaytishi kerak: Hozir 20:50da 1ta non, 5ta lag'mon va 6ta cola mavjud!
+
+
+// Masalani yechimi:
+
+class Shop {
+  constructor(non, qaymoq, moxito) {
+    this.non = non;
+    this.qaymoq = qaymoq;
+    this.moxito = moxito;
+  }
+
+  getTime() {
+    const now = new Date();
+    return `${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`;
+  }
+
+  qoldiq() {
+    console.log(`Hozir ${this.getTime()}da ${this.non}ta non, ${this.qaymoq}ta qaymoq va ${this.moxito}ta moxito mavjud`);
+  }
+
+  sotish(mahsulot, miqdor) {
+    this[mahsulot] -= miqdor;
+    console.log(`Hozir ${this.getTime()}da ${miqdor}ta ${mahsulot} sotildi`);
+  }
+
+  qabul(mahsulot, miqdor) {
+    this[mahsulot] += miqdor;
+    console.log(`Hozir ${this.getTime()}da ${miqdor}ta ${mahsulot} qabul qilindi`);
+  }
 }
 
-console.log(countIntegers("duhf3uhr38239d"));
+// Test qilib ko'ramiz, natija qaytishi kerak
+// Test
+const shop = new Shop(4, 5, 2);
+shop.qoldiq();
+shop.sotish("non", 3);
+shop.qabul("moxito", 4);
+shop.qoldiq();
+
