7bfda501f8114fca3802558f69cc7f23e349213b
johakoha <jakhongirturaev312@gmail.com>
2025-10-02 12:51:22 +0900

    feat:Reja loyihamizning yakuniy bosqich logic

--- app.js
+++ app.js
@@ -52,6 +52,26 @@ app.post("/delete-item", (req, res) => {
     );
 });
 
+app.post("/edit-item", (req,res) => {
+    const data = req.body;
+    console.log(data);
+    db.collection("plans").findOneAndUpdate(
+        {_id: new mongodb.ObjectId(data.id)},
+        { $set: {reja: data.new_input} },
+        function(err, data) {
+            res.json({ state: "success" });
+        }
+    );
+});
+
+
+app.post("/delete-all", (req, res) => {
+    if(req.body.delete_all) {
+        db.collection("plans").deleteMany(function() {
+            res.json({ state:  "hamma rejalar o'chirildi" });
+        });
+    }
+});
 
 
 app.get("/", function (req, res) {
@@ -69,5 +89,4 @@ app.get("/", function (req, res) {
 });
 
 module.exports = app;
-
 
